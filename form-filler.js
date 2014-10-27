0134b37352bfd87b753ae7d7c44b4e14c9365b86
Trevor Feeney <trev@feens.org>
2014-10-27 09:08:30 -0700

    email settings

--- form-filler.js
+++ form-filler.js
@@ -1,4 +1,6 @@
-function fillForm(e){
+var settings = {};
+
+function handleMessage(e){
   if (e.name == 'fillForm') {
     var firstName = /first(.*)name/
     var lastName = /last(.*)name/
@@ -9,6 +11,7 @@ function fillForm(e){
     var country = /country/
     var address = /address/
     var phone = /phone/
+    var password = /password/
 
 
     var inputs = document.getElementsByTagName("input");
@@ -30,18 +33,39 @@ function fillForm(e){
         }else if(inputs[i].name.match(phone)) {
           safari.self.tab.dispatchMessage("getDataForType", {type:"phone", inputName: inputs[i].name});
         }else if(inputs[i].name.match(email)){
-          safari.self.tab.dispatchMessage("getDataForType", {type:"email", inputName: inputs[i].name});
+          safari.self.tab.dispatchMessage("getDataForType", {type:"email", inputName: inputs[i].name, options: {domain: settings.email_domain, prefix: settings.email_prefix}});
+        }else if(inputs[i].name.match(password)){
+          inputs[i].value = settings.default_password;
         }
       }
     }
 
+    var selects = document.getElementsByTagName("select");
+
+    for(var i = 0; i < selects.length; i++) {
+      console.log(selects[i].name);
+      if(selects[i].value == 0 || selects[i].value == ''){
+        var random = Math.floor(Math.random() * (selects[i].options.length-2)) + 1;
+        selects[i].value = selects[i].options[random].value;
+        // selects[i].onchange();
+        console.log(random);
+        console.log(selects[i].options[random].value);
+      }
+
+    }
+
   } else if(e.name == 'fillFormInput') {
-    el = document.getElementsByName(e.message.inputName)[0];
-    el.value = e.message.value;
+    var el = document.getElementsByName(e.message.inputName)[0];
+    var value = e.message.value;
+    if(typeof(e.message.options.prefix) != 'undefined') value = e.message.options.prefix + value;
+    el.value = value;
+  } else if(e.name == 'updateSettings') {
+    settings = e.message;
   }
 
 }
 // alert('auto test');
 if (window === window.top) {
-  safari.self.addEventListener("message", fillForm, false);
+  safari.self.addEventListener("message", handleMessage, false);
+  safari.self.tab.dispatchMessage("getSettings");
 }
